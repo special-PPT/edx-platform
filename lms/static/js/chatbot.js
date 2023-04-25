c4d13910ba03163c6cc1ea54d40954e043f77450
Hang Wei <30917686+special-PPT@users.noreply.github.com>
2023-04-25 18:11:07 -0400

    comment get time and reorganize chatbot.scss

--- lms/static/js/chatbot.js
+++ lms/static/js/chatbot.js
@@ -17,22 +17,22 @@ for (let i = 0; i < coll.length; i++) {
     });
 }
 
-function getTime() {
-    let today = new Date();
-    hours = today.getHours();
-    minutes = today.getMinutes();
+// function getTime() {
+//     let today = new Date();
+//     hours = today.getHours();
+//     minutes = today.getMinutes();
 
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
+//     if (hours < 10) {
+//         hours = "0" + hours;
+//     }
 
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
+//     if (minutes < 10) {
+//         minutes = "0" + minutes;
+//     }
 
-    let time = hours + ":" + minutes;
-    return time;
-}
+//     let time = hours + ":" + minutes;
+//     return time;
+// }
 
 // Gets the first message
 function firstBotMessage() {
